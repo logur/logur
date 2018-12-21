349d9cc9c886afac61b98c6cde59766fa42bf6d0
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-12-21 15:16:53 +0100

    Use new assertion function in test assertion

--- internal/loggertesting/assertion.go
+++ internal/loggertesting/assertion.go
@@ -10,23 +10,8 @@ import (
 func AssertLogEvents(t *testing.T, expected logur.LogEvent, actual logur.LogEvent) {
 	t.Helper()
 
-	if expected.Level != actual.Level {
-		t.Errorf("expected log levels to be equal\ngot:  %s\nwant: %s", actual.Level, expected.Level)
-	}
-
-	if expected.Line != actual.Line {
-		t.Errorf("expected log lines to be equal\ngot:  %q\nwant: %q", actual.Line, expected.Line)
-	}
-
-	if len(expected.Fields) != len(actual.Fields) {
-		t.Errorf("expected log fields to be equal\ngot:  %v\nwant: %v", actual.Fields, expected.Fields)
-	}
-
-	for key, value := range expected.Fields {
-		if actual.Fields[key] != value {
-			t.Errorf("expected log fields to be equal\ngot:  %v\nwant: %v", actual.Fields, expected.Fields)
-
-			break
-		}
+	err := logur.AssertLogEventsEqual(expected, actual)
+	if err != nil {
+		t.Error(err)
 	}
 }
