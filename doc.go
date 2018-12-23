be09109f7f0f5dd8672163f11ba1115821ffbfa0
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-12-24 00:40:45 +0100

    Add example link to godoc

--- doc.go
+++ doc.go
@@ -1,6 +1,8 @@
 /*
 Package logur is an opinionated collection of logging best practices.
 
-See the readme for detailed documentation: https://github.com/goph/logur/blob/master/README.md
+Check the readme for detailed documentation: https://github.com/goph/logur/blob/master/README.md
+
+Also, check the example package for detailed examples: https://godoc.org/github.com/goph/logur/example
 */
 package logur
