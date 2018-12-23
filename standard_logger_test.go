a5cd16baafaf6eb35ce68a9e412fa2cc09c293d1
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-12-23 16:02:56 +0100

    Add standard error logger

--- standard_logger_test.go
+++ standard_logger_test.go
@@ -32,3 +32,28 @@ func TestNewStandardLogger(t *testing.T) {
 
 	logtesting.AssertLogEvents(t, event, *(logger.LastEvent()))
 }
+
+func TestNewStandardErrorLogger(t *testing.T) {
+	logger := NewTestLogger()
+	stdLogger := NewStandardErrorLogger(logger, "", 0)
+
+	const msg = "message"
+
+	stdLogger.Println(msg)
+
+	// Wait for the written data to reach the logger
+	for i := 0; i < 3; i++ {
+		if logger.Count() > 0 {
+			break
+		}
+
+		time.Sleep(time.Duration((i+1)*10) * time.Millisecond)
+	}
+
+	event := LogEvent{
+		Level: Error,
+		Line:  msg,
+	}
+
+	logtesting.AssertLogEvents(t, event, *(logger.LastEvent()))
+}
