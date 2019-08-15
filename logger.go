efbfaa2e9d4fc4c8799bfa54dfdbb2599b09dc64
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-08-15 14:50:13 +0200

    Compact code

--- logger.go
+++ logger.go
@@ -176,9 +176,7 @@ type PrintLogger struct {
 
 // NewPrintLogger returns a new PrintLogger.
 func NewPrintLogger(logger LogFunc) *PrintLogger {
-	return &PrintLogger{
-		logger: logger,
-	}
+	return &PrintLogger{logger}
 }
 
 // NewErrorPrintLogger returns a new PrintLogger that logs everything on error level.
@@ -209,32 +207,20 @@ type MessageLogger struct {
 
 // NewMessageLogger returns a new MessageLogger instance.
 func NewMessageLogger(logger Logger) *MessageLogger {
-	return &MessageLogger{
-		logger: logger,
-	}
+	return &MessageLogger{logger}
 }
 
 // Trace logs a Trace level event.
-func (l *MessageLogger) Trace(msg string) {
-	l.logger.Trace(msg)
-}
+func (l *MessageLogger) Trace(msg string) { l.logger.Trace(msg) }
 
 // Debug logs a Debug level event.
-func (l *MessageLogger) Debug(msg string) {
-	l.logger.Debug(msg)
-}
+func (l *MessageLogger) Debug(msg string) { l.logger.Debug(msg) }
 
 // Info logs a Info level event.
-func (l *MessageLogger) Info(msg string) {
-	l.logger.Info(msg)
-}
+func (l *MessageLogger) Info(msg string) { l.logger.Info(msg) }
 
 // Warn logs a Warn level event.
-func (l *MessageLogger) Warn(msg string) {
-	l.logger.Warn(msg)
-}
+func (l *MessageLogger) Warn(msg string) { l.logger.Warn(msg) }
 
 // Error logs a Error level event.
-func (l *MessageLogger) Error(msg string) {
-	l.logger.Error(msg)
-}
+func (l *MessageLogger) Error(msg string) { l.logger.Error(msg) }
