58d2483795be7f8c8fb7a8f9eb037ebab415d809
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-08-15 14:43:52 +0200

    Relocate noop logger

--- logger.go
+++ logger.go
@@ -42,6 +42,21 @@ type Fields map[string]interface{}
 // LogFunc is a function recording a log event.
 type LogFunc func(msg string, fields ...map[string]interface{})
 
+type noopLogger struct{}
+
+// NewNoopLogger creates a no-op logger that discards all received log events.
+// Useful in examples and as a fallback logger.
+func NewNoopLogger() Logger {
+	return &noopLogger{}
+}
+
+func (*noopLogger) Trace(msg string, fields ...map[string]interface{}) {}
+func (*noopLogger) Debug(msg string, fields ...map[string]interface{}) {}
+func (*noopLogger) Info(msg string, fields ...map[string]interface{})  {}
+func (*noopLogger) Warn(msg string, fields ...map[string]interface{})  {}
+func (*noopLogger) Error(msg string, fields ...map[string]interface{}) {}
+
+
 // WithFields returns a new logger instance that attaches the given fields to every subsequent log call.
 func WithFields(logger Logger, fields map[string]interface{}) Logger {
 	if len(fields) == 0 {
