e532dbac8021f06bda24c4094077af3ad1bdfaac
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-08-15 15:15:58 +0200

    Move example package

--- doc.go
+++ doc.go
@@ -3,6 +3,6 @@ Package logur is an opinionated collection of logging best practices.
 
 Check the readme for detailed documentation: https://github.com/goph/logur/blob/master/README.md
 
-Also, check the example package for detailed examples: https://godoc.org/github.com/goph/logur/example
+Also, check the example package for detailed examples: https://godoc.org/github.com/goph/logur/examples
 */
 package logur
