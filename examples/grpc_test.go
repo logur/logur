3788e7b9611b59e067132ee2493dbad0b6866a57
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-08-16 13:08:41 +0200

    Move gRPC logger integration to the main module

--- examples/grpc_test.go
+++ examples/grpc_test.go
@@ -4,13 +4,12 @@ import (
 	"google.golang.org/grpc/grpclog"
 
 	"github.com/goph/logur"
-	"github.com/goph/logur/integrations/grpcloglog"
 )
 
 func Example_grpclog() {
 	logger := logur.NewNoopLogger() // choose an actual implementation
 
-	grpclog.SetLoggerV2(grpcloglog.New(logger))
+	grpclog.SetLoggerV2(logur.NewGRPCV2Logger(logger))
 
 	// Output:
 }
