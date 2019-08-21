39765bb22868b9db90392834f567e58941d8e620
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-08-21 20:29:10 +0200

    Relocate zerolog adapter

--- adapters/zerologadapter/logger.go
+++ adapters/zerologadapter/logger.go
@@ -3,66 +3,15 @@ package zerologadapter
 
 import (
 	"github.com/rs/zerolog"
+	zerologadapter "logur.dev/adapter/zerolog"
 )
 
 // Logger is a logur adapter for zerolog.
-type Logger struct {
-	logger zerolog.Logger
-}
+// Deprecated: use logur.dev/adapter/zerolog.Logger instead.
+type Logger = zerologadapter.Logger
 
 // New returns a new logur compatible logger with zerolog as the logging library.
+// Deprecated: use logur.dev/adapter/zerolog.New instead.
 func New(logger zerolog.Logger) *Logger {
-	return &Logger{
-		logger: logger,
-	}
-}
-
-// Trace implements the logur.Logger interface.
-func (l *Logger) Trace(msg string, fields ...map[string]interface{}) {
-	// Fall back to Debug
-	l.Debug(msg, fields...)
-}
-
-// Debug implements the logur.Logger interface.
-func (l *Logger) Debug(msg string, fields ...map[string]interface{}) {
-	event := l.logger.Debug()
-
-	if len(fields) > 0 {
-		event.Fields(fields[0])
-	}
-
-	event.Msg(msg)
-}
-
-// Info implements the logur.Logger interface.
-func (l *Logger) Info(msg string, fields ...map[string]interface{}) {
-	event := l.logger.Info()
-
-	if len(fields) > 0 {
-		event.Fields(fields[0])
-	}
-
-	event.Msg(msg)
-}
-
-// Warn implements the logur.Logger interface.
-func (l *Logger) Warn(msg string, fields ...map[string]interface{}) {
-	event := l.logger.Warn()
-
-	if len(fields) > 0 {
-		event.Fields(fields[0])
-	}
-
-	event.Msg(msg)
-}
-
-// Error implements the logur.Logger interface.
-func (l *Logger) Error(msg string, fields ...map[string]interface{}) {
-	event := l.logger.Error()
-
-	if len(fields) > 0 {
-		event.Fields(fields[0])
-	}
-
-	event.Msg(msg)
+	return zerologadapter.New(logger)
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/hashicorp/go-hclog v0.9.2
 	github.com/onsi/ginkgo v1.9.0 // indirect
 	github.com/onsi/gomega v1.6.0 // indirect
-	github.com/rs/zerolog v1.11.0
+	github.com/rs/zerolog v1.15.0
 	github.com/sirupsen/logrus v1.4.2
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0
@@ -17,4 +17,5 @@ require (
 	logur.dev/adapter/kit v0.1.0
 	logur.dev/adapter/logrus v0.1.0
 	logur.dev/adapter/zap v0.1.0
+	logur.dev/adapter/zerolog v0.1.0
 )
